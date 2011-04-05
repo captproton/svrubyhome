78bcc70bac6e107119081986bf868e432057e3e2
captproton <carl@captproton.com>
2011-04-05 10:14:58 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Svrubyhome::Application.config.secret_token = '64eba9d59a0b348844608e5f1ab08e0a662afb64bac2ccad7b20c782dea683b441874968f58a88300791b7a402b81f3a9c78f3a082d9e7e3b5257f89b6bbbd8d'
